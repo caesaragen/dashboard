2af84f6fba3e850b8f7eb4deeaa1620d36919fcd
Joel Aduma <caesaragen@gmail.com>
2022-08-01 14:14:10 +0300

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+
+import './App.css';
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <h1 className='underline text-3xl'>App</h1>
   )
 }
 
