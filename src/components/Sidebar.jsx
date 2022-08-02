533465d177f9eb7a1494f4148fbd8038a209c585
Joel Aduma <caesaragen@gmail.com>
2022-08-02 13:13:40 +0300

    complete sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,17 +7,44 @@ import { links } from '../data/dummy';
 
 const Sidebar = () => {
   const activeMenu = true;
+  const activeLink ='flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
+  const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2 text-md  text-gray-700'; 
   return (
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
       {activeMenu && (<>
         <div className='flex  justify-between items-center'>
-          <Link to='/' onClick={() => { }} className='text-2xl font-bold'>
+          <Link to='/' onClick={() => { }} className='text-xl flex font-extrabold tracking-tight items-center gap-3 ml-3 mt-4 dark:text-white text-slate-900'>
             <SiShopware className='text-gray-600' />  <span>Dookah</span>
           </Link>
+          <TooltipComponent content="Menu"position="BottomCenter">
+            <button type="button" onClick={()=>{}} className="text-3xl p-3 hover:drop-shadow-xl rounded-full hover:bg-light-gray mt-4 md:hidden">
+              <MdOutlineCancel/>
+            </button>
+          </TooltipComponent>
       </div>
-      </>)
-        
-      }
+      <div className="mt-10">
+        {links.map((item)=>(
+          <div key={item.title}>
+            <p className="text-gray-400 mt-4 m-3 uppercase">
+              {item.title}
+            </p>
+            {item.links.map((link) => (
+              <NavLink
+              to={`/${link.name}`}
+              key={link.name}
+              onClick={() => { }}
+              className={({isActive})=> (isActive ? activeLink : normalLink)}
+              >
+              {link.icon}
+              <span className='capitalize'>
+                {link.name}
+              </span>
+                </NavLink>
+            ))}
+            </div>
+        ))}
+        </div>
+      </>)}
     </div>
   )
 }
