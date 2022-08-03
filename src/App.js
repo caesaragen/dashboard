71e496c58a619a89d2ee7b3942941d17bfaf1658
Joel Aduma <caesaragen@gmail.com>
2022-08-03 14:51:11 +0300

    create theme ui

--- src/App.js
+++ src/App.js
@@ -37,6 +37,8 @@ const App = () => {
                             <Navbar />
                         </div>
                         <div>
+                        <ThemeSettings />
+
                             <Routes>
                                 {/* Dashboard */}
                                 <Route path="/" element={<Ecommerce />} />
