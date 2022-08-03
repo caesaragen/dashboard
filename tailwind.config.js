c3da34fd0bf1c2f60aa2c50e53dd17d6ec2298b4
caesaragen <caesaragen@gmail.com>
2022-08-03 19:43:06 +0300

    fix layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,7 +40,7 @@ module.exports = {
             },
             backgroundImage: {
                 'hero-pattern':
-                    "url('https://demos.wrappixel.com/premium-admin-templates/react/flexy-react/main/static/media/welcome-bg-2x-svg.25338f53.svg')",
+                    "url('https://img.freepik.com/free-vector/e-wallet-money-transfer-concept-mobile-wallet-internet-banking-e-wallet-credit-card-mobile-app-accounting-investments-safe-online-internet-transaction-vector-illustration_1150-55440.jpg?w=900&t=st=1659544877~exp=1659545477~hmac=bbfdc4b892df8e1686f13eb2f93818b5f7aa3eb4af0d71b0422d5ca11b28e20e')",
             },
         },
     },
