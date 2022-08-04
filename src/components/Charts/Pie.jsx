8645d86c66c75a475392811655be0ede1335bfb2
caesaragen <caesaragen@gmail.com>
2022-08-04 14:20:41 +0300

    add pie chart

--- src/components/Charts/Pie.jsx
+++ src/components/Charts/Pie.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from 'react';
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  DateTime,
+  Legend,
+  Tooltip,
+  PieSeries,
+  AccumulationChartComponent,
+  AccumulationSeriesCollectionDirective,
+  AccumulationSeriesDirective,
+  AccumulationLegend,
+  AccumulationTooltip, AccumulationDataLabel
+} from '@syncfusion/ej2-react-charts';
+import {
+  pieChartData
+} from '../../data/dummy';
+import { useContextState } from '../../contexts/ContextProvider';
 
-const Pie = () => {
+const PieChart = () => {
+   const { setColor, setMode, currentMode, currentColor, setThemeSettings } = useContextState();
   return (
-    <div>Pie</div>
+    <div>
+      <AccumulationChartComponent id='pie-chart' 
+        title='Expenses by Category'
+        legendSettings={{ visible: false }}
+        enableSmartLabels={true}
+        enableAnimation={false}
+        center={{ x: '50%', y: '50%' }}
+        tooltip={{ enable: true, format: '${point.x} : <b>${point.y}%</b>' }}
+        background={currentMode === 'Dark' ? '#33373e' : '#ffffff'}
+      >
+        <Inject services={[AccumulationLegend, PieSeries, AccumulationTooltip, AccumulationDataLabel]} />
+        <AccumulationSeriesCollectionDirective>
+          <AccumulationSeriesDirective dataSource={pieChartData}  xName='x' yName='y'
+            explode={true} explodeOffset='10%' explodeIndex={0}
+            dataLabel={{
+              visible: true,
+              position: 'Inside', name: 'text',
+              font: {
+                fontWeight: '600'
+              }
+            }}
+            radius='90%'
+          >
+          </AccumulationSeriesDirective>
+        </AccumulationSeriesCollectionDirective>
+      </AccumulationChartComponent>
+    </div>
   )
 }
 
-export default Pie
+export default PieChart
--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React from 'react';
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  DateTime,
+  Legend,
+  SplineAreaSeries
+} from '@syncfusion/ej2-react-charts';
+import {
+  areaCustomSeries,
+  areaPrimaryYAxis,
+  areaPrimaryXAxis
+} from '../../data/dummy';
+import { useContextState } from '../../contexts/ContextProvider';
+import {Header} from '../../components';
+
 
 const Area = () => {
+  const { currentMode } = useContextState();
   return (
-    <div>Area</div>
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <Header category='Area' title='Inflation Rate in percentage'/>
+      <ChartComponent
+        id='line-chart'
+        height='420px'
+        primaryXAxis={areaPrimaryXAxis}
+        primaryYAxis={areaPrimaryYAxis}
+        chartArea={{ border: { width: 0 } }}
+        tooltip={{ enable: true }}
+        background={currentMode === 'Dark' ? '#33373e' : '#ffffff'}
+      >
+        <Inject services={[SplineAreaSeries, DateTime, Legend]} />
+        <SeriesCollectionDirective>
+          {areaCustomSeries.map((item, index) => (
+            <SeriesDirective key={index} {...item} />
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
   )
 }
 
